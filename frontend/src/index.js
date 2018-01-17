aa0cd31c93f7bd6c79effedaba89075093160b73
Marvin Clerge <clergemarvin@gmail.com>
2018-01-17 12:27:48 -0500

    frontend commit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,30 +1,58 @@
+const store = {notes: {}}
 
+document.addEventListener('DOMContentLoaded', () => {
+  const sideBar = document.querySelector('#side-bar')
 
-document.addEventListener("DOMContentLoaded", () => {
-  let x;
   fetch("http://localhost:3000/api/v1/notes")
-    .then( response => {return response.json()} ).then(data => {
+  .then(response => response.json())
+  .then(notes => {
 
-
-// BEGIN RENDERING SIDEBAR
-      const storeNotes = {}
-      const sidebar = document.getElementById("side-bar")
-      // const sidebar = document.getElementById("side-bar")
-      let listOfNotes = [];
-      for (let element in data) {
-        let newNote = new Note(data[element].title, data[element].body);
-        storeNotes[data[element].id] = newNote;
-        listOfNotes.push(newNote.createSidebarDiv());
+    sideBar.addEventListener('click', handleShowFullNote)
+    function handleShowFullNote(event){
+      if (event.target.nodeName === "P" || event.target.nodeName === "H3") {
+        store.notes[event.target.parentElement.id].showFullNote();
+      } else {
+        store.notes[event.target.id].showFullNote();
       }
+    }
 
-      sidebar.innerHTML = listOfNotes.join("");
-
-// click on note summary
-    sidebar.addEventListener("click", handleNoteSummary)
-
+    for (const note of notes) {
+      new Note(note.title, note.body, note.id).createSidebarDiv()
+    }
 
 
-// end of fetch block
-    })
 
+
+  });
 })
+
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   let x;
+//   fetch("http://localhost:3000/api/v1/notes")
+//     .then( response => {return response.json()} ).then(data => {
+//
+//
+// // BEGIN RENDERING SIDEBAR
+//       const storeNotes = {}
+//       const sidebar = document.getElementById("side-bar")
+//       // const sidebar = document.getElementById("side-bar")
+//       let listOfNotes = [];
+//       for (let element in data) {
+//         let newNote = new Note(data[element].title, data[element].body);
+//         storeNotes[data[element].id] = newNote;
+//         listOfNotes.push(newNote.createSidebarDiv());
+//       }
+//
+//       sidebar.innerHTML = listOfNotes.join("");
+//
+// // click on note summary
+//     sidebar.addEventListener("click", handleNoteSummary)
+//
+//
+//
+// // end of fetch block
+//     })
+//
+// })
--- frontend/src/note.js
+++ frontend/src/note.js
@@ -1,14 +1,41 @@
 const Note = (() => {
-  let id = 0
   return class Note {
-    constructor(title,body){
-      this.id = ++id;
+    constructor(title, body, id){
+      this.id = id;
       this.title = title;
       this.body = body;
+      store.notes[this.id] = this
     }
 
     createSidebarDiv(){
-      return `<div class="note-summary" data-id=${this.id}><h3>${this.title}</h3><p>${this.body.slice(0,25)}</p></div>`
+      const sideBar = document.querySelector('#side-bar')
+
+      let noteWrapper = document.createElement('div')
+      noteWrapper.id = this.id;
+      noteWrapper.className = 'note-summary';
+
+      let noteTitle = document.createElement('h3')
+      noteTitle.innerHTML = this.title;
+
+      let noteBody = document.createElement('p')
+      noteBody.innerHTML = `${this.body.slice(0, 25)} <br>`
+
+      sideBar.append(noteWrapper)
+      noteWrapper.append(noteTitle)
+      noteWrapper.append(noteBody)
+    }
+
+    showFullNote(){
+      const currentNote = document.querySelector('#current-note')
+      currentNote.innerHTML = ''
+      let noteTitle = document.createElement('h1')
+      noteTitle.innerHTML = this.title;
+
+      let noteBody = document.createElement('p')
+      noteBody.innerHTML = `${this.body}`
+
+      currentNote.append(noteTitle)
+      currentNote.append(noteBody)
     }
   }
 })()
